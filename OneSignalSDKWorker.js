26fd1b69ad541328f7b742a76a4a8ec6ad4316b0
babyshop-express-web <babyshop860@gmail.com>
2026-08-09 11:10:22 -0300

    Update OneSignalSDKWorker.js

--- OneSignalSDKWorker.js
+++ OneSignalSDKWorker.js
@@ -1,1 +1,9 @@
 importScripts("https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.sw.js");
+
+self.addEventListener('install', function(e){
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', function(e){
+  self.clients.claim();
+});
